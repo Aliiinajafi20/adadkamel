6617b578569813c04e6e994beb30c5b66df61980
shirin-ux <shirin.alizadeh19@gmail.com>
2020-07-24 12:08:08 +0430

    add new file

--- adad kamel.py
+++ adad kamel.py
@@ -18,18 +18,14 @@ class myThread (threading.Thread):
       process_data(self.name, self.q)
       print ("Exiting " + self.name)
 
-
 def adad_kamel(n):
     sum = 1
-
-
     for i in range(2, (int)(math.sqrt(n)) + 1):
         if (n % i == 0):
             sum += i
             if (i != (n / i)):
                 sum += n / i
         i += 1
-
     if (sum == n):
         return True
     return False
@@ -45,7 +41,6 @@ def process_data(threadName, q):
          queueLock.release()
          time.sleep(1)
 
-
 n = int(input("please enter number: "))
 Thread = int(input("please enter thread: "))
 
@@ -53,7 +48,6 @@ for j in range(n):
     if (adad_kamel(j)):
         print(j)
 lentgh = int(n / Thread)
-
 
 threadList = ["Thread-1", "Thread-2", "Thread-3"]
 nameList = ["lentgh One", "lentgh Two", "lentgh Three", "lentgh Four", "lentgh Five"]
